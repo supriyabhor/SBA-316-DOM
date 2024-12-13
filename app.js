7380ff1ff234c223cc35550817f0b4e926a1589d
supriyabhor <supriyanaikwadi89@gmail.com>
2024-12-13 15:23:34 -0500

    Word Guess Game using DOM

---
+++ app.js
@@ -0,0 +1,99 @@
+let wordData= [
+   {
+      hint : "Which planet is known as the “Blue Planet”?",
+      word : "Earth"
+   },
+   {
+      hint : "Bill Gates is the founder of which company?",
+      word : "Microsoft"
+   },
+   {
+        hint : "Larry Page is the CEO of which company?",
+        word : "Google"
+   },
+   {    
+        hint : "What is Hewlett Packard (HP) originally known for?",
+        word : "Printers"
+   },
+   {
+        hint : "In the story of Snow White, how many dwarfs are there?",
+        word : "Seven"
+   },
+   {
+        hint : "What is the chemical symbol for the element mercury?",
+        word : "Hg"
+   },
+   {
+       hind : "How many colors are there in a rainbow?",
+       word : "Seven"
+   },
+   {
+        hind : "What animal is known for its long trunk and large ears?",
+        word : "Elephant"
+   },
+   {
+         hind : "Which planet is known as the “Red Planet”?",
+        word : "Mars"
+   },
+   {
+        hind :  "What part of the atom has no electric charge?",
+        word : "Neutron"
+   },
+   {
+        hind : "What animals are pearls found in?",
+        word : "Oysters"
+   },
+   {
+        hind : "How many molecules of oxygen does ozone have?",
+        word : "Three"
+   },
+   {
+        hind : "Which planet has the most gravity?",
+        word : "Jupiter"
+   },
+   {
+        hind : "In which US city is Broadway located?",
+        word : "New York City"
+   },
+   {
+        hind :  "Which country invented tea?",
+        word : "China"
+   },
+   {
+        hind : "Which organ has four chambers?",
+        word : "heart"
+   },
+   {
+        hind : "Which continent is the largest?",
+        word : "Asia"
+   },
+    {
+        hind : "Which country is known as the Land of the Rising Sun?",
+        word : "Japan"
+    },
+    {
+        hind : "What animal is on Levi’s logo?",
+        word : "Horse"
+    },
+       
+];
+
+
+let attemptCount = 4 ;
+
+const hintTab = document.querySelector(".hint");
+const totalAttemptTab = document.querySelector(".totalAttempt");
+const inputTextTab = document.getElementById ("#inputText");
+const buttonCheck = document.getElementById ("#checkBtn");
+const buttonReset = document.getElementById ("#resetBtn");
+
+
+ 
+
+
+function buttonFun ()
+{
+  
+   
+  
+}
